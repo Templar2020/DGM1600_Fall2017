57e6a0310e06a7ed86380f4b86de33baeb65ae8c
Unknown <preston.powell@uvu.edu>
2017-08-30 20:11:11 -0600

    Added Unity Project files - go team rocket

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000; 
+		public float fuel = 0.01f;
+
+		private string name = "Bob";
+
+			
+
+	}
+}
